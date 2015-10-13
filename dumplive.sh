bd341194d648d9d09d806d5cf99acefb00c670d8
Piers Beckley <piers@fatpigeons.com>
2015-10-13 15:48:05 +0100

    improvements to dumplive.sh. mostly in the form of a warning.

--- dumplive.sh
+++ dumplive.sh
@@ -2,10 +2,11 @@
 
 # This lives on the remote server and is called via ssh to get a dump of the database there
 # Username & Password need to be defined in .my.cnf 
+# Uncomment and fill out OWNER and chown lines if the script is being run as root, for some insane reason
 
 DUMPFILE="/path/to/create/dumpfile.sql"
 DATABASE="database"
-OWNER="owner"
+# OWNER="owner"
 
 mysqldump $DATABASE > "$DUMPFILE"
-chown $OWNER:$OWNER "$DUMPFILE"
+# chown $OWNER:$OWNER "$DUMPFILE"
