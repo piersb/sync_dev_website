cf33dc9602c44373db53b8c0d0c65e3a6b25a575
Piers Beckley <piers@fatpigeons.com>
2015-11-11 16:55:43 +0000

    updated dumplive.sh

--- dumplive.sh
+++ dumplive.sh
@@ -1,16 +1,9 @@
 #! /bin/bash 
 
 # This lives on the remote server and is called via ssh to get a dump of the database there
-# Username & Password need to be defined in (eg) ~/.sync_dev_website/FILENAME.my.cnf 
-# where filename is set in the config file on the local machine (usually = sitename)
-# Uncomment and fill out OWNER and chown lines if the script is being run as root, for some insane reason
-# Compression requires gzip to be installed on remote machine
+# Username & Password need to be defined in SITENAME.my.cnf 
 
+DUMPFILE="/var/lib/mysql/dumpfile.sql"
+DATABASE="stage_dezeen_live"
 
-MYSQLCNF="/path/to/my.cnf"
-DUMPFILE="/path/to/create/dumpfile.sql"
-DATABASE="database"
-# OWNER="owner"
-
-mysqldump --defaults-file=$MYSQLCNF $DATABASE > "$DUMPFILE"
-#chown $OWNER:$OWNER "$DUMPFILE"
+mysqldump --defaults-file="/var/lib/mysql/.sync_dev_website/$1.my.cnf" $DATABASE > "$DUMPFILE"
